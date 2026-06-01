1838b86c485bdebcefd2091343a1bf9c36a407fb
Erik Lundh <erik.l.lundh@gmail.com>
2026-06-01 14:40:23 +0200

    feat(usb): add CDC device descriptors and tusb config
    One CDC interface, VID=0xCafe PID=0x0001, product string
    "Pico CDC Test Rig". CFG_TUSB_RHPORT0_MODE required by
    TinyUSB 0.16+ when tusb_init() is called with no arguments.
    
    Refs: phase/01-cdc-enumerate

--- pico-cdc-test-rig/src/main.c
+++ pico-cdc-test-rig/src/main.c
@@ -1,17 +1,10 @@
-#include "pico/stdlib.h"
+#include "bsp/board_api.h"
+#include "tusb.h"
 
 int main(void) {
-#ifdef PICO_DEFAULT_LED_PIN
-    const uint LED = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED);
-    gpio_set_dir(LED, GPIO_OUT);
+    board_init();
+    tusb_init();
     while (true) {
-        gpio_put(LED, 1);
-        sleep_ms(250);
-        gpio_put(LED, 0);
-        sleep_ms(250);
+        tud_task();
     }
-#else
-    while (true) { tight_loop_contents(); }
-#endif
 }
